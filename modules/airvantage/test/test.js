c5103813fe194c21962a87e292b47ab27bdc319d
aymandi <ayman.diab@elementn.com>
2016-12-16 16:49:10 +0200

    Fix the results returned by the test.js api for the testCharts.js to read

--- modules/airvantage/test/test.js
+++ modules/airvantage/test/test.js
@@ -1,3 +1,7 @@
+//Make sure to set the correct configuration in the config.js file before testing.
+//This api script reads the latest 10 history messages representing the (blackInkPrinted and colorInkPrinted) level sent from the "AV Phone Demo" mobile application
+//This api is used in the testCharts.js. It build the appropriate data structure for the testCharts.js to draw the line chart
+
 var config = require("../config.js");
 var system = require("../system.js");
 var result = [["Request", "Black Ink Level", "Color Ink Level"]];
@@ -5,20 +9,32 @@ var result = [["Request", "Black Ink Level", "Color Ink Level"]];
 var airvantageClient = require("../airvantageClient.js");
 var tokenManager = require("../authorization/TokenManager.js");
 
+//retrieve and set the access tocken as a global variable to be use by system.js in order to call the tokenManager.callApi() function
 var accessTokenSetResult = tokenManager.getAccessTokenFromCredentials();
+
+//This method returns a paginated list of systems with their complete details.  
 var systemResults = system.findSystems();
 
+//Choose the uid of the first system available
 var systemUID = systemResults.items[0].uid;
+
+//Retrieve all the messages of the current system described by its systemUID
 var systemMessages = system.getSystemMessages(systemUID);
 var count = 0;
-for(var x=0; x<10; x++){
+
+//loop on all the latest 10 messages
+for(var x=systemMessages.messages.length-11; x<systemMessages.messages.length-1; x++){
 	var systemMessageUID = systemMessages.messages[x].uid;  
+  	
+  //retrieve the details of each message
 	var messageDetails = system.getSystemMessageDetails(systemUID, systemMessageUID);
     if(messageDetails["data"] && messageDetails["data"]["phone.custom.3"] && messageDetails["data"]["phone.custom.4"]){
       count ++;
-      var blackInkPrinted = messageDetails["data"]["phone.custom.3"][0]["value"];
-      var colorInkPrinted = messageDetails["data"]["phone.custom.4"][0]["value"];  
+      //read the colored and black ink levels from the messages sent
+      var blackInkPrinted = parseInt(messageDetails["data"]["phone.custom.3"][0]["value"]);
+      var colorInkPrinted = parseInt(messageDetails["data"]["phone.custom.4"][0]["value"]);  
       result.push([count, blackInkPrinted, colorInkPrinted]);
     }
 }
+//return the results as expected by the google chart set in the testCharts.js
 return result;
