052ce320f34d8a268c4dc7816af89cb5224e3e38
Karim Saikali <karim.saikali@elementn.com>
2017-11-09 14:41:44 +0200

    Added getLatestDataPoint() and sendApplicativeCommand()

--- modules/airvantage/system.js
+++ modules/airvantage/system.js
@@ -87,7 +87,7 @@ function getSystemMessageDetails(systemUID, messageUID){
 
 /**
  * Return last data points sent by the given device
- * @method getLastDataPoints
+ * @function getLastDataPoints
  * @param {String} uid: the device's uid
  * @return { <metric> : {value:<some_value>, timestamp:<timestamp>}, <metric>:{...}. ... }
  */
@@ -100,6 +100,52 @@ function getLastDataPoints(uid) {
         method: "GET"
     };
 
+    var result = airvantageClientObject.callApi(httpObj);
+    return result
+}
+
+/**
+ * @function sendApplicativeCommand
+ * @param {Object} [dto]
+ * @param {Boolean} [dto.notify]: optional, if true, sends an email notification to the user when the operation finishes.
+ * @param {String} [dto.callback] : optional callback url that receives updates on the operation satus. Defaults to config.callback
+ * @param {Number} [dto.scheduledTime] : optional, timestamp, Date time when the operation will be launched. If not specified starts immediately.
+ * The time must be less than 30 days.
+ * @param {Number} [dto.timeout] : "timeout", optional, The date for which the operation is considered as expired. Defaults to 7 days
+ * @param {Boolean} [dto.requestConnection] : if true, will try to notify the system
+ * @param {String} [dto.protocol] : if specified, message will be sent thought this protocol. Defaults to Airvantage configuration
+ * @param {Object} [dto.systems] 
+ * @param {String} [dto.systems.label] : all systems linked to the label will be incorporated to the operation
+ * @param {Array} [dto.systems.uids] : ["uid1", ...],
+ * @param {String} [dto.systems.dependsOn]
+ * @param {String} [dto.systems.operation] : optional,  current operation starts after the operations specified by this fied are finished. 
+ * @param {String} [dto.systems.operation.states] : optiona, one of ["FAILURE", "SUCCESS", "CANCELLED"]
+ * @param {String} [dto.commandId] : the command id (e.g. "start")
+ * @param {Object} [dto.parameters] : {key/value}
+ * @return {Object} {operation: <operation_id>}
+ * @throws {Error}
+ */
+function sendApplicativeCommand(dto) {
+    
+    if (!dto || !dto.commandId || !dto.systems || (!dto.systems.uids && !dto.systems.label)) {
+        
+        throw {
+            errorCode: "Invalid_Parameter",
+            errorDetail: "dto.commandId and dto.systems.uids or dto.systems.label are mandatory"
+        };
+    }
+    
+    dto.callback = dto.callback ? dto.callback : config.callback;
+    var airvantageClientObject = new airvantageClient.AirvantageClient();
+    var url = config.airVantageApiUrl + "operations/systems/command";
+    var httpObj = {
+        url: url,
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        params: dto
+    };
     var result = airvantageClientObject.callApi(httpObj);
     return result
 }
